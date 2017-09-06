631ea4b6fecc30c6f8a9c5039caaebaac5c93b16
hatfield-c <chatfield@ymail.com>
2017-09-05 23:54:09 -0500

    [TRN-1] Initial Commit

---
+++ Workflow Training/J-Uva/workflow_training.asm
@@ -0,0 +1,24 @@
+# A program that says hello!
+
+.text main:
+	#output: Prints str_welcomeMsg
+	la $a0, msg
+	li $v0, 4
+	syscall
+
+	#method: Add 4 & 2 together and place it in $t0
+	li $t1 4
+	li $t2 2
+	add $t0, $t1, $t2
+	
+	#output: Prints the sum of $t0 & $t1 from $t0.
+	move $a0 $t0
+	li $v0, 1
+	syscall
+
+	#method: exits the program
+	li $v0 10,
+	syscall
+		
+.data
+	msg: 	.asciiz 	"Welcome! This program will calculate two integer values that you enter.\n2 + 4 = "
