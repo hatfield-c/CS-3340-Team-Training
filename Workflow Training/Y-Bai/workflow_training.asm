7a8b47c12c78d5e934e01df3db4cab88b98a8ef3
Y Bai <hmax5023@gmail.com>
2017-10-18 22:10:52 -0500

    4 + 4 = 8

--- Workflow Training/Y-Bai/workflow_training.asm
+++ Workflow Training/Y-Bai/workflow_training.asm
@@ -6,12 +6,12 @@
 	li $v0, 4
 	syscall
 
-	#method: Add 4 & 2 together and place it in $t0  hell yea
+	#method: Add 4 & 4 together and place it in $t0  
 	li $t1 4
-	li $t2 2
+	li $t2 4
 	add $t0, $t1, $t2
 	
-	#output: Prints the sum of $t0 & $t1 from $t0. hell yea
+	#output: Prints the sum of $t0 & $t1 from $t0. 
 	move $a0 $t0
 	li $v0, 1
 	syscall
@@ -21,4 +21,4 @@
 	syscall
 		
 .data
-	msg: 	.asciiz 	"Welcome! This program will calculate two integer values that you enter.\n2 + 4 = "
+	msg: 	.asciiz 	"Welcome! This program will calculate two integer values that you enter.\n 4 + 4 = "
