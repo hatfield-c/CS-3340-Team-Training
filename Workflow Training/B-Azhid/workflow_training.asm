99c0a62d716c0cb7a3fabb33c16cb42b9590a0cf
Basit Zahid <basit.zahid@yahoo.com>
2017-11-14 16:42:42 -0600

    Update workflow_training.asm

--- Workflow Training/B-Azhid/workflow_training.asm
+++ Workflow Training/B-Azhid/workflow_training.asm
@@ -6,9 +6,9 @@
 	li $v0, 4
 	syscall
 
-	#method: Add 4 & 2 together and place it in $t0
-	li $t1 4
-	li $t2 2
+	#method: Add 69 & 69 together and place it in $t0
+	li $t1 69
+	li $t2 69
 	add $t0, $t1, $t2
 	
 	#output: Prints the sum of $t0 & $t1 from $t0.
@@ -21,4 +21,4 @@
 	syscall
 		
 .data
-	msg: 	.asciiz 	"Welcome! This program will calculate two integer values that you enter.\n2 + 4 = "
+	msg: 	.asciiz 	"Welcome! This program will calculate two integer values that you enter.\n69 + 69 = "
